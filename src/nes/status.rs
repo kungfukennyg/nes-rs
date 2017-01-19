212a9712b8fbc5939a4764d8706c4498ff4d4c8c
Kenny <kennethbgoodin@gmail.com>
2017-01-19 01:36:08 -0500

    Clean up CPU status flags struct/impl, add missing negative flag

--- src/nes/status.rs
+++ src/nes/status.rs
@@ -1,10 +1,9 @@
-const CARRY_BIT: u8 = 1 << 0;
-const ZERO_FLAG: u8 = 1 << 1;
-const INTERRUPT_FLAG: u8 = 1 << 2;
-const DECIMAL_FLAG: u8 = 1 << 3;
-const BREAK_FLAG: u8 = 1 << 4;
-const OVERFLOW_FLAG: u8 = 1 << 6;
-const NEGATIVE_FLAG: u8 = 1 << 7;
+const CARRY_BIT: u8 = 0;
+const ZERO_FLAG: u8 = 1;
+const INTERRUPT_FLAG: u8 = 2;
+const BREAK_FLAG: u8 = 4;
+const OVERFLOW_FLAG: u8 = 6;
+const NEGATIVE_FLAG: u8 = 7;
 
 #[derive(Default, Debug)]
 pub struct Status {
@@ -20,20 +19,32 @@ pub struct Status {
     // (V) overflow flag (set if an invalid two's complement result was obtained from previous
     // instruction)
     overflow_flag: bool,
-    // (N) negative flag (set if sign bit on last instruction
+    // (N) negative flag (set if sign bit on last instruction is set)
+    negative_flag: bool
 }
 
 impl Status {
 
     pub fn set_negative(&mut self, value: u8) {
-        self.zero_flag = (value & 0x80) == 0;
+        self.negative_flag = (value & 0x80) == 0;
     }
 
     pub fn set_zero(&mut self, value: u8) {
-        self.carry_flag = value == 0;
+        self.zero_flag = value == 0;
     }
 
     pub fn set_carry(&mut self, value: bool) {
         self.carry_flag = value;
+    }
+
+    pub fn value(&self) {
+        let value = 0;
+        value |= self.carry_flag << CARRY_BIT;
+        value |= self.zero_flag << ZERO_FLAG;
+        value |= self.interrupt_flag << INTERRUPT_FLAG;
+        value |= self.break_flag << BREAK_FLAG;
+        value |= self.overflow_flag << OVERFLOW_FLAG;
+        value |= self.negative_flag << NEGATIVE_FLAG;
+
     }
 }
