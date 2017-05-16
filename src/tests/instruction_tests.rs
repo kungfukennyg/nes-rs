b8891e744247060281355edf32ac4c29241ac2f2
Kenny <kennethbgoodin@gmail.com>
2017-05-16 00:03:19 -0400

    Add BPL instruction test

--- src/tests/instruction_tests.rs
+++ src/tests/instruction_tests.rs
@@ -2311,4 +2311,20 @@ mod tests {
 
         assert!(cpu.registers.program_counter == 0x0104);
     }
+
+    // BPL
+
+    #[test]
+    fn test_bpl() {
+        let mut cpu = Cpu::new();
+
+        cpu.registers.program_counter = 0x0100;
+
+        cpu.memory.store(0x0100, 0x10);
+        cpu.memory.store(0x0101, 0x02);
+
+        cpu.execute_instruction();
+
+        assert!(cpu.registers.program_counter == 0x0104);
+    }
 }
