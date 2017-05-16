49a94291678bf7670d080a6bc487ca7c06e7e81f
Kenny <kennethbgoodin@gmail.com>
2017-05-15 23:50:12 -0400

    Add BEQ instruction test

--- src/tests/instruction_tests.rs
+++ src/tests/instruction_tests.rs
@@ -2261,4 +2261,21 @@ mod tests {
 
         assert!(cpu.registers.program_counter == 0x0104);
     }
+
+    // BEQ
+
+    #[test]
+    fn test_beq() {
+        let mut cpu = Cpu::new();
+
+        cpu.registers.processor_status |= cpu::ZERO_FLAG;
+        cpu.registers.program_counter = 0x0100;
+
+        cpu.memory.store(0x0100, 0xf0);
+        cpu.memory.store(0x0101, 0x02);
+
+        cpu.execute_instruction();
+
+        assert!(cpu.registers.program_counter == 0x0104);
+    }
 }
